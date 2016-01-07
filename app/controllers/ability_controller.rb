03f436722146918e268db74b9f0f3035eedc3bdb
Chae Cramb <chaecramb@gmail.com>
2016-01-07 15:29:33 +0000

    tweak abilities

--- app/controllers/ability_controller.rb
+++ app/controllers/ability_controller.rb
@@ -1,8 +1,8 @@
 class AbilityController < ApplicationController
 
   def show
-    if params[:id] == 1
-      redirect_to action: "story", id: 5
+    if params[:id] == '1'
+      redirect_to story_path(1)
     end
   end
 
